555dbfbd5b09c30d001fa5a946ed246571d21247
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2026-08-25 13:48:08 +0200

    all

---
+++ app/main.py
@@ -0,0 +1,151 @@
+from fastapi import FastAPI, status
+from pydantic import BaseModel, EmailStr, Field
+from fastapi import HTTPException
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "Hello FastAPI!"}
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello, Dymitr!"}
+
+
+# @app.get("/books")
+# def get_books():
+#     return {
+#         "books": [
+#             {"id": 1, "title": "Gdzie kończy się imię"},
+#             {"id": 2, "title": "Metro 2033"},
+#         ]
+#     }
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    return {
+        "book_id": book_id,
+        "title": "Gdzie kończy się imię",
+        "author": "Dymitr Dworakowski",
+        "year": 2022,
+        "available": True
+
+    }
+
+
+
+@app.get("/books")
+def get_books(limit: int = 10, available: bool = True):
+    return {
+        "limit": limit,
+        "available": available
+    }
+
+@app.get("/about")
+def about():
+    return {"name": "Dymitr", "role": "Network Administrator", "learning": "FastAPI"}
+
+
+@app.get("/search")
+def search_books(query: str | None = None):
+    return {
+        "query": query
+    }
+
+
+
+
+class Book(BaseModel):
+    title: str
+    author: str
+    year: int
+    available: bool = True
+
+
+@app.post("/books")
+def create_book(book: Book):
+    return {
+        "message": f"Книгу {book.title} створено ",
+        "author": book.author
+    }
+
+
+class Movie(BaseModel):
+        title: str
+        director: str
+        year: int
+        available: bool = True
+        
+movies = [
+    {
+        "id": 1,
+        "title": "Inception",
+        "director": "Christopher Nolan",
+        "year": 2010
+    },
+    {
+        "id": 2,
+        "title": "Interstellar",
+        "director": "Christopher Nolan",
+        "year": 2014
+    }
+]
+
+@app.get("/movies/{movie_id}")
+def get_movie(movie_id: int):
+    
+    for movie in movies:
+        if movie["id"] == movie_id:
+            return movie
+   
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Фільм не знайдено"
+    )
+
+@app.post("/movies", status_code=status.HTTP_201_CREATED)
+def create_movie(movie: Movie):
+    return {
+        "message": f"Фільм {movie.title} створено",
+        "director": movie.director
+    }
+
+@app.get("/movies")
+def get_movies(limit: int = 10, available: bool = True):
+    return {
+        "limit": limit,
+        "available": available
+    }
+
+@app.delete("/movies/{movie_id}", status_code=status.HTTP_200_OK)
+def delete_movie(movie_id: int):
+    for movie in movies:
+        if movie["id"] == movie_id:
+            movies.remove(movie)
+
+            return {
+                "message": f"Фільм {movie['title']} видалено"
+            }
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Фільм не знайдено"
+    )
+
+class User(BaseModel):
+    username: str = Field(min_length=3, max_length=20)
+    email: EmailStr
+    age: int = Field(ge=18, le=120)
+    full_name: str | None = None
+    is_active: bool = True
+
+@app.post("/users", status_code=status.HTTP_201_CREATED)
+def create_user(user: User):
+    return {
+        "message": f"Користувача {user.username} створено",
+        "email": user.email,
+        "age": user.age
+    }
+
---
+++ movie.js
@@ -0,0 +1,14 @@
+movies = [
+    {
+        "id": 1,
+        "title": "Inception",
+        "director": "Christopher Nolan",
+        "year": 2010
+    },
+    {
+        "id": 2,
+        "title": "Interstellar",
+        "director": "Christopher Nolan",
+        "year": 2014
+    }
+]
